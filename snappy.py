5ec89f48ea4900bb49ebd356b95ad640e9cb6a53
David Senack <david.senack@gmail.com>
2024-11-20 00:27:50 -0500

    remove distro release name

--- snappy.py
+++ snappy.py
@@ -15,7 +15,7 @@ def get_packages():
     return pkg_arr
 
 def get_pkg_name(pkg_line: str) -> str:
-    tmp = pkg_line.split(" ")
+    tmp = pkg_line.split("/")
     pkg_name_dirty = tmp[0]
     pkg_name_clean = pkg_name_dirty.split(",")[0]
     return pkg_name_clean
@@ -28,6 +28,8 @@ def get_pkg_version(pkg_line: str) -> str:
 
 pkg_list = get_packages()
 
-for pkg in pkg_list[1:10]:
+for pkg in pkg_list[1:5]:
     pkg_name = get_pkg_name(pkg)
     pkg_version = get_pkg_version(pkg)
+    print(pkg_name)
+    print(pkg_version)
