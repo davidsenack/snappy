71620f24b791d935ee5721939d06db149cd0f8f8
David Senack <david.senack@gmail.com>
2024-11-19 23:59:36 -0500

    fix get_package function to append packages to output array

--- snappy.py
+++ snappy.py
@@ -5,7 +5,21 @@ import subprocess
 def get_packages():
     get_packages_cmd = "apt list --installed"
     tmp = subprocess.run(get_packages_cmd, shell=True, capture_output=True, text=True)
-    package_list = tmp.stdout.strip()
-    return package_list
+    pkg_list = tmp.stdout.strip()
+    new_pkg_list = pkg_list.split('\n')
 
-print(get_packages())
+    pkg_arr = []
+    for pkg in new_pkg_list:
+        pkg_arr.append(pkg)
+    
+    return pkg_arr
+
+def get_pkg_version(output_line):
+    output = string.split(output_line, " ")
+    return output
+
+
+pkg_list = get_packages()
+
+for pkg in pkg_list:
+    print(pkg)
