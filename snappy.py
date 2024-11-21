55727e6c892ea2b74eb22a27605ca9980e5ee77c
David Senack <david.senack@gmail.com>
2024-11-21 13:42:24 -0500

    add get_snapshot_packages function

--- snappy.py
+++ snappy.py
@@ -4,20 +4,32 @@ import subprocess
 import datetime
 import os 
 
-def get_current_packages():
+def get_current_pkgs():
     get_packages_cmd = "apt list --installed"
     tmp = subprocess.run(get_packages_cmd, shell=True, capture_output=True, text=True)
     pkg_list = tmp.stdout.strip()
     new_pkg_list = pkg_list.split('\n')
 
-    pkg_list = []
+    pkg_arr = []
 
     for pkg in new_pkg_list[1:]:
         pkg_name = get_pkg_name(pkg)
         pkg_version = get_pkg_version(pkg)
-        pkg_list.append("{} {}".format(pkg_name, pkg_version))
+        pkg_arr.append("{} {}".format(pkg_name, pkg_version))
 
-    return pkg_list
+    return pkg_arr
+
+def get_snapshot_pkgs(snapshot_file_path):
+    pkg_arr = []
+
+    with open(snapshot_file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+            pkg_name = line.split(" ")[0]
+            pkg_version = line.split(" ")[1]
+            pkg_arr.append("{} {}".format(pkg_name, pkg_version))
+    
+    return pkg_arr
 
 def get_pkg_name(pkg_line: str) -> str:
     tmp = pkg_line.split("/")
@@ -84,8 +96,8 @@ def restore_from_snapshot(snapshot_name):
 
 #create_snapshot()
 snapshot_filename = "2024-11-20_23-18-1732162722.pkg.snapshot"
-print(get_packages())
-
+snapshot_path = "/home/david/.snappy/"
+print(get_snapshot_pkgs(snapshot_path + snapshot_filename))
 
 # We need to write a comparison function for the restore function to use. 
 # It should compare the current installed packages and versions to the snapshot
