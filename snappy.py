1641d378c885623196b2930613a281685a8421ef
David Senack <david.senack@gmail.com>
2024-11-20 23:19:43 -0500

    finalize create snapshot function

--- snappy.py
+++ snappy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import subprocess
+import datetime
+import os 
 
 def get_packages():
     get_packages_cmd = "apt list --installed"
@@ -32,17 +34,32 @@ def install_pkg(pkg_name, pkg_version):
     return
 
 def create_snapshot():
+    # Check for .snappy directory
+    directory = os.path.expanduser("~/.snappy")
+    os.makedirs(directory, exist_ok=True)
+
+    # Set filename based on timestamp
+    current_time = datetime.datetime.now()
+    file_name = current_time.strftime("%Y-%m-%d_%H-%M-%s") + ".pkg.snapshot"
+    
+    # Set full file path
+    file_path = os.path.join(directory, file_name)
+
+    # Get current package data
+    pkg_list = get_packages()
+
+    # Write current pacakge data to output file
+    with open(file_path, "w") as file:
+        for pkg in pkg_list[1:]:
+            pkg_name = get_pkg_name(pkg)
+            pkg_version = get_pkg_version(pkg)
+            file.write("{} {}\n".format(pkg_name, pkg_version))
+
+    print(f"Current package snapshot has been written to '{file_path}'")
+
     return
 
 def restore_from_snapshot():
     return
 
-pkg_list = get_packages()
-
-for pkg in pkg_list[1:5]:
-    pkg_name = get_pkg_name(pkg)
-    pkg_version = get_pkg_version(pkg)
-    #print(pkg_name)
-    #print(pkg_version)
-    install_pkg(pkg_name, pkg_version)
-    # comment comment
+create_snapshot()
